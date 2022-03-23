4f2665f2e1ca573ea8cf6fcc277064ae84d456be
Art <1>
2022-03-23 20:51:27 +0700

    add missing file

---
+++ pages/basket_page.py
@@ -0,0 +1,17 @@
+from pages.base_page import BasePage
+from pages.locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def check_basket(self):
+        self.check_text_in_basket()
+        self.check_empty_basket()
+
+    def check_text_in_basket(self):
+        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY), f"Element {BasketPageLocators.BASKET_EMPTY}" \
+                                                                          f" not found"
+
+    def check_empty_basket(self):
+        assert self.is_disappeared(*BasketPageLocators.BASKET_WITH_PRODUCT), f"Element " \
+                                                                        f"{BasketPageLocators.BASKET_WITH_PRODUCT}" \
+                                                                        f" found"
